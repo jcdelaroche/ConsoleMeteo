a40f100ba3f2e408e047ad214ef26a72af0205b9
Acethephoenix <acethephoenix2001@gmail.com>
2023-11-22 12:26:41 +0100

    modif pour package npm

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const getCoord = require("./getCoord");
 const getWeather = require("./getWeather");
 const city = process.argv[2] || "Paris";
 
-getCoord(city)
-  .then((coord) => getWeather(coord))
-  .then(({current: weather}) => {
-    console.log(`Météo à ${city} :`);
-    console.log(`- Température : ${weather.temperature_2m}°C`);
-    console.log(`- Humidité : ${weather.relative_humidity_2m}%`);
-    console.log(`- Risque de pluie : ${weather.precipitation_probability}%`);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    console.log("Au revoir !");
-  });
+const getMeteo = (address) => {
+  getCoord(city)
+    .then((coord) => getWeather(coord))
+    .then(({current: weather}) => {
+      console.log(`Météo à ${city} :`);
+      console.log(`- Température : ${weather.temperature_2m}°C`);
+      console.log(`- Humidité : ${weather.relative_humidity_2m}%`);
+      console.log(`- Risque de pluie : ${weather.precipitation_probability}%`);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      console.log("Au revoir !");
+    });
+}
 
+module.exports = getMeteo;
